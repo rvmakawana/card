f66150acfe887ae2e0fc7b89b31ff7289e4e4a6c
Ravi Makawana <rvmakawana@gmail.com>
2024-03-09 14:15:54 -0600

    Card Functionality

---
+++ src/main/java/com/bank/card/CardsApplication.java
@@ -0,0 +1,30 @@
+package com.bank.card;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = " Cards microservice REST API Documentation",
+				version = "v1"
+		),
+		externalDocs = @ExternalDocumentation(
+				description = " Cards microservice REST API Documentation"
+
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+}
